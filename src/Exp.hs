8e9f272810e112319885df12730ac7300407c868
Larry Diehl <larrytheliquid@gmail.com>
2015-01-26 17:26:56 -0800

    Basic SK interpreter

--- src/Exp.hs
+++ src/Exp.hs
@@ -7,18 +7,44 @@ import Tree
 
 ----------------------------------------------------------------------
 
-data Comb = S | K | I
+data Comb = S | K
   deriving (Show,Read,Eq,Ord,Enum,Bounded)
-data Exp = App Exp Exp | Var String | Comb Comb
+
+infixl 5 :@:
+data Exp = Exp :@: Exp | Var String | Comb Comb
+  deriving (Show,Read,Eq)
 
 enum :: (Enum a, Bounded a) => [a]
 enum = enumFromTo minBound maxBound
 
 toExp :: Tree Comb -> Exp
-toExp = undefined
+toExp (Branch l r) = toExp l :@: toExp r
+toExp (Leaf c) = Comb c
 
 norm :: Exp -> Exp
-norm = undefined
+norm (Comb S :@: x :@: y :@: z) = norm (x :@: z :@: (y :@: z))
+norm (Comb K :@: x :@: _) = norm x
+-- norm (Comb S :@: (norm -> x) :@: (norm -> y) :@: (norm -> z)) = norm (x :@: z :@: (y :@: z))
+-- norm (Comb K :@: (norm -> x) :@: _) = x
+norm (f :@:  x) = norm f :@: norm x
+norm x@(Comb _) = x
+norm x@(Var  _) = x
+
+_S :: Exp
+_S = Comb S
+
+_K :: Exp
+_K = Comb K
+
+_I :: Exp
+_I = _S :@: _K :@: _K
+
+_T :: Exp
+_T = _S :@: (_K :@: (_S :@: _I)) :@: _K
+
+_T' :: Exp
+_T' = _S :@: (_K :@: (_S :@: _I)) :@: (_S :@: (_K :@: _K) :@: _I)
+
 
 score :: Exp -> Int
 score = undefined
