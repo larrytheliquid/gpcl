4b9d77374cde120443229c782c506711fdb9f635
Larry Diehl <larrytheliquid@gmail.com>
2015-02-11 11:42:54 -0800

    All combinator problems

--- src/Evo.hs
+++ src/Evo.hs
@@ -171,23 +171,53 @@ main = gens solved
 -- http://www.angelfire.com/tx4/cus/combinator/birds.html
 
 solved =
- [ prob "K"  "ab"   $ "a"
- , prob "I"  "a"    $ "a"
- , prob "T"  "ab"   $ "b" :@: "a"
- , prob "B"  "abc"  $ "a" :@: ("b" :@: "c")
- , prob "M"  "a"    $ "a" :@: "a"
- , prob "M2" "ab"   $ "a" :@: "b" :@: ("a" :@: "b")
- -- hard
- , prob "W"  "ab"   $ "a" :@: ("b" :@: "b")
- , prob "D"  "abcd" $ "a" :@: "b" :@: ("c" :@: "d")
- , prob "Q3" "abc"  $ "c" :@: ("a" :@: "b")
- , prob "C"  "abc"  $ "a" :@: "c" :@: "b"
- , prob "B1" "abcd" $ "a" :@: ("b" :@: "c" :@: "d")
- , prob "U"  "ab"   $ "b" :@: ("a" :@: "a" :@: "b")
- ]
-
-unsolved =
- [ prob "Q"  "abc"  $ "b" :@: ("a" :@: "c")
+ [ prob "B"     "abc"     $ "a" :@: ("b" :@: "c")
+ , prob "B1"    "abcd"    $ "a" :@: ("b" :@: "c" :@: "d")
+ , prob "B2"    "abcde"   $ "a" :@: ("b" :@: "c" :@: "d" :@: "e")
+ , prob "B3"    "abcd"    $ "a" :@: ("b" :@: ("c" :@: "d"))
+ , prob "C"     "abc"     $ "a" :@: "c" :@: "b"
+ , prob "D"     "abcd"    $ "a" :@: "b" :@: ("c" :@: "d")
+ , prob "D1"    "abcde"   $ "a" :@: "b" :@: "c" :@: ("d" :@: "e")
+ , prob "D2"    "abcde"   $ "a" :@: ("b" :@: "c") :@: ("d" :@: "e")
+ , prob "E"     "abcde"   $ "a" :@: "b" :@: ("c" :@: "d" :@: "e")
+ , prob "E^"    "abcdefg" $ "a" :@: ("b" :@: "c" :@: "d") :@: ("e" :@: "f" :@: "g")
+ , prob "F"     "abc"     $ "c" :@: "b" :@: "a"
+ , prob "G"     "abcd"    $ "a" :@: "d" :@: ("b" :@: "c")
+ , prob "H"     "abc"     $ "a" :@: "b" :@: "c" :@: "b"
+ , prob "I"     "a"       $ "a"
+ , prob "J"     "abcd"    $ "a" :@: "b" :@: ("a" :@: "d" :@: "c")
+ , prob "K"     "ab"      $ "a"
+ , prob "L"     "ab"      $ "a" :@: ("b" :@: "b")
+ , prob "M"     "a"       $ "a" :@: "a"
+ , prob "M2"    "ab"      $ "a" :@: "b" :@: ("a" :@: "b")
+ , prob "O"     "ab"      $ "b" :@: ("a" :@: "b")
+ , prob "Q"     "abc"     $ "b" :@: ("a" :@: "c")
+ , prob "Q1"    "abc"     $ "a" :@: ("c" :@: "b")
+ , prob "Q2"    "abc"     $ "b" :@: ("c" :@: "a")
+ , prob "Q3"    "abc"     $ "c" :@: ("a" :@: "b")
+ , prob "Q4"    "abc"     $ "c" :@: ("b" :@: "a")
+ , prob "R"     "abc"     $ "b" :@: "c" :@: "a"
+ , prob "S"     "abc"     $ "a" :@: "c" :@: ("b" :@: "c")
+ , prob "T"     "ab"      $ "b" :@: "a"
+ , prob "U"     "ab"      $ "b" :@: ("a" :@: "a" :@: "b")
+ , prob "V"     "abc"     $ "c" :@: "a" :@: "b"
+ , prob "W"     "ab"      $ "a" :@: "b" :@: "b"
+ , prob "W1"    "ab"      $ "b" :@: "a" :@: "a"
+ , prob "I*"    "ab"      $ "a" :@: "b"
+ , prob "W*"    "abc"     $ "a" :@: "b" :@: "c" :@: "c"
+ , prob "C*"    "abcd"    $ "a" :@: "b" :@: "d" :@: "c"
+ , prob "R*"    "abcd"    $ "a" :@: "c" :@: "d" :@: "b"
+ , prob "F*"    "abcd"    $ "a" :@: "d" :@: "c" :@: "b"
+ , prob "V*"    "abcd"    $ "a" :@: "c" :@: "b" :@: "d"
+ , prob "I**"   "abc"     $ "a" :@: "b" :@: "c"
+ , prob "W**"   "abcd"    $ "a" :@: "b" :@: "c" :@: "d" :@: "d"
+ , prob "C**"   "abcde"   $ "a" :@: "b" :@: "c" :@: "e" :@: "d"
+ , prob "R**"   "abcde"   $ "a" :@: "b" :@: "d" :@: "e" :@: "c"
+ , prob "F**"   "abcde"   $ "a" :@: "b" :@: "e" :@: "d" :@: "c"
+ , prob "V**"   "abcde"   $ "a" :@: "b" :@: "e" :@: "c" :@: "d"
+ , prob "KI"    "ab"      $ "b"
+ , prob "KM"    "ab"      $ "b" :@: "b"
+ , prob "C(KM)" "ab"      $ "a" :@: "a"
  ]
 
 ----------------------------------------------------------------------
